500de5a707718e568072822cb192b76a9d2c9e76
Paul David <puddavid@gmail.com>
2021-03-18 19:11:16 -0400

    Enable tracing in verilator - Also cleanup the template somewhat

--- rtl/counter.v
+++ rtl/counter.v
@@ -7,22 +7,26 @@
 /// ## Overview
 ///
 /// This is an example counter which serves as a filler module for the
-/// template.
+/// template. Modules will differ immensely in the level of required
+/// documentation.
 ///
 /// ## IO Ports
 ///
 /// There are only a few ports on this module because it's just a simple
 /// counter.
 ///
-/// Port     | Direction | Bus Width | Description
-/// ---------|:----------|:----------|:-------------
+/// Port     | Direction | Bit Width | Description
+/// ---------|:----------|:----------|:--------------
 /// i_clk    | Input     | 1         | Counter clock
 /// i_rst    | Input     | 1         | Counter reset
-/// o_count  | Output    | 16 (VAR)  | Counter output register
+/// o_count  | Output    | 16 (VAR)  | Counter output
 ///
-/// ## Registers
+/// ## Parameters
 ///
-/// The counter has no runtime configuration registers.
+/// Name      | Description
+/// ----------|:-----------------
+/// CW        | Counter width
+/// MAX_COUNT | Max counter value
 ///
 
 //
@@ -31,9 +35,9 @@
 `default_nettype none
 
 module counter (i_clk, i_rst, o_count);
-    parameter CW = 16;
 
-    localparam MAX_COUNT = 10;
+    parameter CW = 16;
+    parameter MAX_COUNT = 10;
 
     input    wire              i_clk;
     input    wire              i_rst;
