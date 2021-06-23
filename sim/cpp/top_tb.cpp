287f070314c671cccfc962f66c9061b7aa5e5ca3
Paul David <puddavid@gmail.com>
2021-06-23 00:04:59 -0400

    Added trace output to template

--- sim/cpp/top_tb.cpp
+++ sim/cpp/top_tb.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <verilated.h>
+#include <verilated_vcd_c.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,15 +14,20 @@ int
 main (int argc, char* argv[])
 {
     Verilated::commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
 
     vluint64_t num_ticks = 1000;
+    vluint64_t sim_tick = 0;
 
     Vtop_tb top_tb;
 
+    VerilatedVcdC vcd;
+
+    top_tb.trace(&vcd, 99);
     top_tb.i_clk = 1;
     top_tb.i_rst = 0;
 
-    vluint64_t sim_tick = 0;
+    vcd.open("counter.vcd");
 
     printf("[%ld] - Started verilator simulation\n", sim_tick);
 
@@ -36,12 +42,16 @@ main (int argc, char* argv[])
         printf("[%04ld] - i_clk = 0x%x, i_rst = 0x%x, o_count = 0x%x\n",
                sim_tick, top_tb.i_clk, top_tb.i_rst, top_tb.o_count);
 
+        vcd.dump(sim_tick);
+
         if (sim_tick >= num_ticks) break;
     }
 
     printf("[%04ld] - Exited simulation\n", sim_tick);
 
     top_tb.final();
+
+    vcd.close();
 
     return EXIT_SUCCESS;
 }
